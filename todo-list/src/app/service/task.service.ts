0e4092ca24ec0ed934b96b3a36625feb7626aae6
zeherbelkacem <59140588+zeherbelkacem@users.noreply.github.com>
2023-02-10 12:30:44 +0100

    1 file modified

--- todo-list/src/app/service/task.service.ts
+++ todo-list/src/app/service/task.service.ts
@@ -4,15 +4,14 @@ import { Observable } from 'rxjs';
 import { Task } from '../model/task';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskService {
+  constructor(private http: HttpClient) {}
 
-  constructor(
-    private http: HttpClient
-  ) { }
-
-  public getAllTasks(): Observable<Task[]>{
-    return this.http.get<Task[]>('');
+  public getAllTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(
+      'https://zeherbelkacem-super-duper-guacamole-4vr6665wxpv37q74-8086.preview.app.github.dev/api/todo/v1/todos'
+    );
   }
 }
